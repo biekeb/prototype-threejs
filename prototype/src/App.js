691d820d11d477630bc8282d11dc13fbc86b4f7a
biekeb <91184065+biekeb@users.noreply.github.com>
2023-10-23 14:04:55 +0200

    Feat: intential setup

---
+++ prototype/src/App.js
@@ -0,0 +1,20 @@
+import { Canvas } from '@react-three/fiber'
+import Model from './components/Scene'
+import { OrbitControls } from "@react-three/drei";
+import RoomModel from './components/RoomModel'
+import Container from './components/Container';
+
+
+function App() {
+  
+  return (
+  <>
+    <Container/>
+    
+
+    
+  </>
+  );
+}
+
+export default App;
