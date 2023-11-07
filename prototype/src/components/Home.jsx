02bbc7d8d40be9e2ed01f33cb82761168bba7d2d
biekeb <91184065+biekeb@users.noreply.github.com>
2023-11-07 11:04:38 +0100

    style: rechaning the style of the homepage

--- prototype/src/components/Home.jsx
+++ prototype/src/components/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import image from "../images/Group 1.png";
+import image from "../images/Group 5.png";
+import overlay from "../images/Image 14.png";
+import grandma from "../images/grandma.png";
 
 const Home = () => {
   return (
     <div className="home">
-      <h2>Welcome to</h2>
-      <h1>Grandma's Kitchen</h1>
-      <img id="img" src={image} alt="" />
+      {/* <h2>Welcome to</h2>
+      <h1>Grandma's Kitchen</h1> */}
+      <img id="home-img" src={image} alt="" />
+      <img id="grandma-img" src={grandma} alt="" />
+      <img id="overlay" src={overlay} alt="" />
+
     </div>
   );
 };
